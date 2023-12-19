b11d39853c3e416da18890a11ffe21ba59b5e00b
AndreGGW <141270125+AndreGGW@users.noreply.github.com>
2023-12-20 01:42:12 +0300

    add task 3.1.4

---
+++ src/main/java/ru/goryunov/springcource/config/Config.java
@@ -0,0 +1,9 @@
+package ru.goryunov.springcource.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.goryunov")
+public class Config {
+}
